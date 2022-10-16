2381f3daa3cde83276b787d6515057e0962f832d
wahek <wahek1999@mail.ru>
2022-10-16 22:20:18 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@
 // 9 5 3 2
 // 8 4 4 2
 
-Console.WriteLine("Введите количество строк");
-int row = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите количество столбцов");
-int column = int.Parse(Console.ReadLine()!);
-int[,] array0 = GetArray(row, column, 1, 9);
-PrintArray(array0);
-Console.WriteLine();
-int [,] sortedArray = SortedMaxMinArray(array0);
-PrintArray(array0);
+// Console.WriteLine("Введите количество строк");
+// int row = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите количество столбцов");
+// int column = int.Parse(Console.ReadLine()!);
+// int[,] array0 = GetArray(row, column, 1, 9);
+// PrintArray(array0);
+// Console.WriteLine();
+// int [,] sortedArray = SortedMaxMinArray(array0);
+// PrintArray(array0);
 
 int[,] GetArray(int rows, int columns, int minValue, int maxValue)
 {
@@ -140,3 +140,24 @@ void PrintArray3x3(int[,,]array3x3){
         }
     }
 }
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] array1 = GetArray(6, 4, 0, 9);
+PrintArray(array1);
+Console.WriteLine();
+int stringIndex = MinStringSum(array1);
+Console.WriteLine($"Индекс минимальной суммы строки на программистском: {stringIndex}");
+Console.WriteLine($"На человеческом: {stringIndex - 1}");
